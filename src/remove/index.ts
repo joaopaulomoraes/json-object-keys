067f7a611479afe7f42d3335409752f73ed2281f
João Paulo Moraes <joaopaulomoraes@outlook.com>
2023-02-28 16:27:57 -0300

    Fix types name

--- src/remove/index.ts
+++ src/remove/index.ts
@@ -2,7 +2,7 @@ type Key = string
 
 type Keys = Array<Key>
 
-type Object = {
+type JSObject = {
   [key: string | number]: unknown
 }
 
@@ -12,7 +12,10 @@ type Object = {
  * @param keys - A single key or an array of keys
  * @returns A new object without the given keys
  */
-export default function remove<T extends Object>(object: T, keys: Key | Keys): T {
+export default function remove<T extends JSObject>(
+  object: T,
+  keys: Key | Keys
+): T {
   if (typeof keys === 'string') {
     return withString(object, keys)
   }
@@ -20,17 +23,17 @@ export default function remove<T extends Object>(object: T, keys: Key | Keys): T
   return withArray(object, keys)
 }
 
-function withString(object: Object, key: Key) {
+function withString(object: JSObject, key: Key) {
   const regex = new RegExp(`"${key}":[^,}]+,?`, 'g')
   return withRegex(object, regex)
 }
 
-function withArray(object: Object, keys: Keys) {
+function withArray(object: JSObject, keys: Keys) {
   const regex = new RegExp(`"(${keys.join('|')})":[^,}]+,?`, 'g')
   return withRegex(object, regex)
 }
 
-function withRegex(object: Object, regex: RegExp) {
+function withRegex(object: JSObject, regex: RegExp) {
   const stringify = JSON.stringify(object).replace(regex, '')
   return JSON.parse(stringify)
 }
