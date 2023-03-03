1a571e347fc848cb83c2364f9ec336f54ff894ec
João Paulo Moraes <joaopaulomoraes@outlook.com>
2023-03-03 13:44:23 -0300

    Improve regex

--- src/remove/test.ts
+++ src/remove/test.ts
@@ -1,62 +1,184 @@
 import { remove } from '.'
 
+const user = {
+  id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+  name: 'John Doe',
+  category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+  email: 'john@doe.com',
+  age: 29
+}
+
+const userNodes = {
+  ...user,
+  node: [
+    {
+      ...user,
+      node: {
+        ...user,
+        node: {
+          ...user
+        }
+      }
+    },
+    {
+      ...user,
+      node: {
+        ...user
+      }
+    },
+    {
+      ...user,
+      node: {
+        ...user,
+        node: {
+          ...user
+        }
+      }
+    }
+  ]
+}
+
 describe('remove', () => {
-  it('should remove a specific key', () => {
-    expect(remove({ id: 1, foo: 2, bar: 3 }, 'foo')).toStrictEqual({
-      bar: 3,
-      id: 1
+  it('should remove the id from user', () => {
+    expect(remove(user, 'id')).toStrictEqual({
+      name: 'John Doe',
+      category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+      email: 'john@doe.com',
+      age: 29
     })
   })
 
-  it('should remove a specific key from different levels', () => {
-    expect(
-      remove(
+  it('should remove the category from user', () => {
+    expect(remove(user, 'category')).toStrictEqual({
+      id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+      name: 'John Doe',
+      email: 'john@doe.com',
+      age: 29
+    })
+  })
+
+  it('should remove category and email from user', () => {
+    expect(remove(user, ['category', 'email'])).toStrictEqual({
+      id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+      name: 'John Doe',
+      age: 29
+    })
+  })
+
+  it('should remove age from user', () => {
+    expect(remove(user, 'age')).toStrictEqual({
+      id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+      name: 'John Doe',
+      category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+      email: 'john@doe.com'
+    })
+  })
+
+  it('should remove id and age from user', () => {
+    expect(remove(user, ['id', 'age'])).toStrictEqual({
+      name: 'John Doe',
+      category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+      email: 'john@doe.com'
+    })
+  })
+
+  it('should remove id from userNodes', () => {
+    expect(remove(userNodes, 'id')).toStrictEqual({
+      name: 'John Doe',
+      category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+      email: 'john@doe.com',
+      age: 29,
+      node: [
         {
-          id: 1,
-          foo: 1,
-          children: {
-            id: 2,
-            foo: 2,
-            children: {}
+          name: 'John Doe',
+          category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+          email: 'john@doe.com',
+          age: 29,
+          node: {
+            name: 'John Doe',
+            category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+            email: 'john@doe.com',
+            age: 29,
+            node: {
+              name: 'John Doe',
+              category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+              email: 'john@doe.com',
+              age: 29
+            }
           }
         },
-        'foo'
-      )
-    ).toStrictEqual({
-      children: {
-        children: {},
-        id: 2
-      },
-      id: 1
+        {
+          name: 'John Doe',
+          category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+          email: 'john@doe.com',
+          age: 29,
+          node: {
+            name: 'John Doe',
+            category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+            email: 'john@doe.com',
+            age: 29
+          }
+        },
+        {
+          name: 'John Doe',
+          category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+          email: 'john@doe.com',
+          age: 29,
+          node: {
+            name: 'John Doe',
+            category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+            email: 'john@doe.com',
+            age: 29,
+            node: {
+              name: 'John Doe',
+              category: '65bf5579-710d-4f56-9907-8c0bb1b2f0d2',
+              email: 'john@doe.com',
+              age: 29
+            }
+          }
+        }
+      ]
     })
   })
 
-  it('should remove multiple keys', () => {
-    expect(remove({ id: 1, foo: 2, bar: 3, baz: [] }, ['foo', 'bar'])).toStrictEqual({ baz: [], id: 1 })
-  })
-})
-
-it('should remove multiple keys from different levels', () => {
-  expect(
-    remove(
-      {
-        id: 1,
-        foo: 1,
-        bar: 1,
-        children: {
-          id: 2,
-          foo: 2,
-          bar: 2,
-          children: {}
+  it('should remove id and node from userNodes', () => {
+    expect(remove(userNodes, ['email', 'category', 'age'])).toStrictEqual({
+      id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+      name: 'John Doe',
+      node: [
+        {
+          id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+          name: 'John Doe',
+          node: {
+            id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+            name: 'John Doe',
+            node: {
+              id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+              name: 'John Doe'
+            }
+          }
+        },
+        {
+          id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+          name: 'John Doe',
+          node: {
+            id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+            name: 'John Doe'
+          }
+        },
+        {
+          id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+          name: 'John Doe',
+          node: {
+            id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+            name: 'John Doe',
+            node: {
+              id: 'e059d01a-7082-4b63-9c70-997491cdcf7c',
+              name: 'John Doe'
+            }
+          }
         }
-      },
-      ['foo', 'bar']
-    )
-  ).toStrictEqual({
-    children: {
-      children: {},
-      id: 2
-    },
-    id: 1
+      ]
+    })
   })
 })
